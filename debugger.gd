68ec0c42a2e5e39f24484c0a4f78c62d66c8edfe
Seb-Cheneb <bucovala.sebastian@protonmail.com>
2025-06-14 20:50:39 +0300

    update

--- debugger.gd
+++ debugger.gd
@@ -2,5 +2,24 @@
 class_name Debugger extends RefCounted
 
 
-func get_class_name() -> String:
-	return get_script().get_path().get_file().get_basename()
+func get_class_name(object: Object) -> String:
+	return object.get_script().get_path().get_file().get_basename()
+
+
+func get_calling_method() -> String:
+	var stack = get_stack()
+	if stack.size() >= 3:  # Skip get_calling_method() and log_info()
+		return stack[2]["function"]
+	return "unknown_method"
+
+
+func info(_is_debugging: bool, _class: Object, _message: String) -> void:
+	if not _is_debugging:
+		return
+	print_rich("[color=orange]", get_class_name(_class), "[/color].[color=yellow]", get_calling_method(), "[/color] :: ", _message)
+
+
+func warning(_is_debugging: bool, _class: Object, _message: String) -> void:
+	if not _is_debugging:
+		return
+	print_rich("[color=orange]", get_class_name(_class), "[/color].[color=yellow]", get_calling_method(), "[/color] :: [color=red]", _message, "[/color]")
